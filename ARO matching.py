2b8ee7e87cee997cdd464bc276ebf2fad9b4e14e
Huayu Qin <46234762+HawaiiChin@users.noreply.github.com>
2020-12-06 20:23:09 +0800

    Update ARO matching.py

--- ARO matching.py
+++ ARO matching.py
@@ -1,5 +1,12 @@
 from pronto import Ontology
 
 cl = Ontology("http://purl.obolibrary.org/obo/cl.obo")
+```#for ARGs here (ARO for antibiotic resistance ontology--aro.owl from https://card.mcmaster.ca/download)
+
 aro = Ontology.from_obo_library("aro.owl")
+
+```#exploring ontology
 cf = aro['confers_resistance_to_antibiotic']
+t = aro['ARO:XXXXXXX']
+list(t.objects(cf))
+list(t.superclasses())
