b809379d7e340997475d6d6520a0fbc9d11d7f97
Huayu Qin <46234762+HawaiiChin@users.noreply.github.com>
2020-12-06 20:44:57 +0800

    Update ARO matching.py

--- ARO matching.py
+++ ARO matching.py
@@ -10,3 +10,8 @@ cf = aro['confers_resistance_to_antibiotic']
 t = aro['ARO:XXXXXXX']
 list(t.objects(cf))
 list(t.superclasses())
+list(t.subclasses())
+
+```#load resfinder sequences for matching
+import pandas as pd
+resfinder_seq = pd.read_txt("sequence.txt", sep=" ", header=None)
