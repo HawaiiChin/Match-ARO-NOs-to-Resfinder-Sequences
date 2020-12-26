23af26783f600313f905841d589f16d723630ae6
Huayu Qin <46234762+HawaiiChin@users.noreply.github.com>
2020-12-26 13:29:09 +0800

    Update ARO matching.py

--- ARO matching.py
+++ ARO matching.py
@@ -1,21 +1,19 @@
 from pronto import Ontology
 
 cl = Ontology("http://purl.obolibrary.org/obo/cl.obo")
-```#for ARGs here (ARO for antibiotic resistance ontology--aro.owl from https://card.mcmaster.ca/download)
+#for ARGs here (ARO for antibiotic resistance ontology--aro.owl from https://card.mcmaster.ca/download)
 
 aro = Ontology.from_obo_library("aro.owl")
 
-```#exploring ontology
+#exploring ontology
 cf = aro['confers_resistance_to_antibiotic']
 t = aro['ARO:XXXXXXX']
 list(t.objects(cf))
 list(t.superclasses())
 list(t.subclasses())
 
-```#load resfinder sequences for matching
+#load resfinder sequences for matching
 import pandas as pd
 resfinder_seq = pd.read_csv("beta-lactam.csv", sep=" ", header=None)
-* if permission denied, change access 
-$ chmod 755 beta-lactam.csv
 
 #now match aro info to resfinder_seq
