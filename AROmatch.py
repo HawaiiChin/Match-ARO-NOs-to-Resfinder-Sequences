e2cc255d71ab1999bdc12e648047a580653576bf
Huayu Qin <46234762+HawaiiChin@users.noreply.github.com>
2020-12-28 17:17:04 +0800

    Add files via upload

---
+++ AROmatch.py
@@ -0,0 +1,29 @@
+import pronto
+from glob import glob
+aro = pronto.Ontology('aro.owl')
+
+name2id = {}
+for t in aro.terms():
+    name2id[t.name] = t.id
+    for syn in t.synonyms:
+        assert syn.scope == 'EXACT'
+        name2id[syn.description] = t.id
+
+name2id = {k.lower():v for k,v in name2id.items()}
+
+matched = []
+unmatched = []
+for fname in glob('notes.txt'):
+    for line in open(fname):
+            gene = line.split(':')[0]
+            if gene.lower() in name2id:
+                matched.append(gene)
+            elif gene[:3] == 'bla':
+                gene = gene[4:]
+                if gene.lower() in name2id:
+                    matched.append(gene)                
+            else:
+                unmatched.append(gene)
+
+frac = (len(matched)/(len(unmatched)  + len(matched)))
+print(f'Matched {len(matched)} of {len(matched)+len(unmatched)} ({frac:.2%}) of identifiers')
